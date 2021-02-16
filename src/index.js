707a28bc28beb6e722aa1e9c902205e6ef867706
Manoj Kumar <manojkumar@Manojs-MacBook-Pro.local>
2021-02-16 21:36:36 +0530

    Optimizations SourceMap and stuff.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert('how are you...');
+alert('how are you da dae...');
+
+
+// var a = 0;
+// alert(`addition result is ${a + 1}`
+
+
+// var a = new Date();
+// alert(`addition result is ${a + 1}`
